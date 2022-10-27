a40f3e668205c00f1a0bb4d57f5e4ac1fea168bd
John Rey <adioskid@mail.ru>
2022-10-27 13:11:17 +0300

    chat

--- src/Absolute.java
+++ src/Absolute.java
@@ -1,4 +1,7 @@
 public class Absolute {
     public static void main(String[] args) {
+        int a = 12, b = 27, c = 44, d = 15, e = 9;
+        int result = a * (b + (c - d * e));
+        System.out.println(result * (-1));
     }
 }
